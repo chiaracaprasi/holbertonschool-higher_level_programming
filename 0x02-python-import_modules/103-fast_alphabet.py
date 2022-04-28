befc68a020378822b84765c33c227095d30640e6
Chiara Caprasi <caprasichiara@gmail.com>
2022-04-27 21:54:17 -0700

    fix: requirements

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-
-    import string
-
-    uppercase_alphabets = string.ascii_uppercase
-    print(uppercase_alphabets)
+import string
+print(string.ascii_uppercase)
