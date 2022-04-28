296ea01277b47b898450941fe97c9508b44cfcf3
Chiara Caprasi <caprasichiara@gmail.com>
2022-04-27 23:04:24 -0700

    fix: style issues

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     ac = len(sys.argv)
     if ac != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
@@ -15,20 +15,20 @@ if __name__ == "__main__":
 
     if operator == '+':
         print(f"{a} + {b} =", add(a, b))
-        exit (0)
+        exit(0)
 
     if operator == '-':
         print(f"{a} - {b} =", sub(a, b))
-        exit (0)
+        exit(0)
 
     if operator == '*':
         print(f"{a} * {b} =", mul(a, b))
-        exit (0)
+        exit(0)
 
     if operator == '/':
         print(f"{a} / {b} =", div(a, b))
-        exit (0)
+        exit(0)
 
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit (1)
+        exit(1)
