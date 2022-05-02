919fcaae04a93412714c07ff60db1696b8c6dcfc
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-01 21:12:40 -0700

    frix: change method for adding tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
-
 def add_tuple(tuple_a=(), tuple_b=()):
-
     if len(tuple_a) == 0:
         a = (0, 0)
     elif len(tuple_a) == 1:
@@ -17,6 +14,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     else:
         b = tuple_b
 
-    result = tuple(map(lambda i, j: i + j, a, b))
-
+    result = (a[0] + b[0], a[1] + b[1])
     return result
