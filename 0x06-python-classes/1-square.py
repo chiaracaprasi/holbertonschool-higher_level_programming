02608f8d7354ad4ca2fa954b8507a755155f1393
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-16 23:02:24 -0700

    fix: doc style issue

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a Square.
+"""Represents a Square.
     Private instance attribute: size
-    Instantiation with size (no type/value verification).
-    """
+    Instantiation with size (no type/value verification)."""
 
+
+class Square:
+    """Represents a class square."""
     def __init__(self, size):
         """ Initialise the data."""
         self.__size = size
