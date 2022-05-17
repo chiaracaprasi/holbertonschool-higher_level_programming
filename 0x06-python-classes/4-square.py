235e25a6ee46f882b4a9ca210e6865e0e3aa1ef8
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-16 19:16:25 -0700

    build: add script for task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Square:
+    """Represents a Square.
+    Private instance attribute: size
+    Instantiation with optional size
+    Public instance method: area"""
+
+    def __init__(self, size=0):
+        """ Initialise the data."""
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Retrieves the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Sets the size."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Returns the current square area."""
+        return self.__size ** 2
