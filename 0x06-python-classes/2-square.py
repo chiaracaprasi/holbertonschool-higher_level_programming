16b7bb38cdccd74921b5f1647c75fc2a3a49932d
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-16 18:19:39 -0700

    build: add script for task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """Represents a Square.
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)."""
+
+    def __init__(self, size=0):
+        """ Initialise the data."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
