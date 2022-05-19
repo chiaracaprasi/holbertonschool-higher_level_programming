653b886b032817e6f595dc1156ca7a9fc7c5c61d
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-18 20:46:39 -0700

    build: add script for task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module add-integer
+Adds two integer together
+"""
+
+
+def add_integer(a, b=98):
+    """Returns the addition of a and b,
+    a and b must be integers or floats otherwise error will occur
+    a and b must be first casted to integers if they are float
+    """
+    if not isinstance(a, int) or not isinstance(a, float):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, int) or not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    if isinstance(a, float):
+        a = int(a)
+
+    if isinstance(b, float):
+    b = int(b)
+
+    return a + b
