bf5f520977f31e41b366beb21d7a867fa0964e23
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-18 21:27:41 -0700

    fix: line count for documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """
+
 Module add-integer
 Adds two integer together
+
 """
 
 
 def add_integer(a, b=98):
     """Returns the addition of a and b,
     a and b must be integers or floats otherwise error will occur
-    a and b must be first casted to integers if they are float
-    """
+    a and b must be first casted to integers if they are float """
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
 
