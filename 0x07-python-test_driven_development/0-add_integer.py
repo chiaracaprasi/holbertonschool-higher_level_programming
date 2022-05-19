c841ed926a2d3f52701d036df1d6f52cabf7e441
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-18 21:15:56 -0700

    fix: logic and indentation issue

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,16 +10,16 @@ def add_integer(a, b=98):
     a and b must be integers or floats otherwise error will occur
     a and b must be first casted to integers if they are float
     """
-    if not isinstance(a, int) or not isinstance(a, float):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
 
-    if not isinstance(b, int) or not isinstance(b, float):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
 
     if isinstance(a, float):
         a = int(a)
 
     if isinstance(b, float):
-    b = int(b)
+        b = int(b)
 
     return a + b
