50161b6fd179a9dfa28d4ec54f950d8af6068689
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-23 17:34:19 +1000

    build: add more tests for task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -2,18 +2,61 @@
 """Unittest for max_integer([..])
 """
 import unittest
+from xml.dom.minidom import Element
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """TestCase for the max_integer function."""
     
+    def test_module_docstring(self):
+        """Tests for module docsting"""
+        mod = __import__('6-max_integer').__doc__
+        self.assertTrue(len(mod) > 1)
+
+    def test_function_docstring(self):
+        """Tests for funstion docstring"""
+        func = max_integer.__doc__
+        self.assertTrue(len(func) > 1)
+        
     def test_regular_use(self):
         """Test with a regular list of ints: should return the max result"""
         l = [1, 2, 3, 4, 5]
         result = max_integer(l)
         self.assertEqual(result, 5)
      
+    def test_none(self):
+        """Tests for none as argument"""
+        with self.assertRaises(TypeError):
+            max_integer(None)
+            
+    def test_non_int(self): 
+        """Tests for a non-int"""
+        l = [1, 2, 3, "Hello", 5]
+        with self.assertRaises(TypeError):
+            max_integer(l)
+        
+    def test_empty_list(self):
+        """Tests result of empty list []"""
+        l = []
+        self.assertIsNone(max_integer(l))   
+        
+    def test_same_values(self):
+        """Tests result of same values in list []"""
+        l = [1, 5, 5, 5]
+        result = max_integer(l)
+        self.assertEqual(result, 5)
+    
+    def test_same_values(self):
+        """Tests result of same values in list []"""
+        l = [1, 5, 5, 5]
+        result = max_integer(l)
+        self.assertEqual(result, 5)
 
+    def test_all_negative(self):
+        """Tests for list with all negative numbers"""
+        l = [-1, -2, -3, -4, -5]
+        self.assertEqual(max_integer(l), -1)
+        
         
 if __name__ == "__main__":
     unittest.main()
