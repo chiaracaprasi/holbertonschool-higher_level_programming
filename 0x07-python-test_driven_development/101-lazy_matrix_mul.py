93fa8161a2a4959e8b39260153607069a678f1ec
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-23 16:15:56 +1000

    build: add script for task 7

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Defines lazy_matrix function
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ Returns the matrix multiplication of two matrices"""   
+    return mat.mul(m_a, m_b)
