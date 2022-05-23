2c916ecdc517c8c226d692473e795ba104ada3ef
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-22 21:57:31 -0700

    fix: switch order of height and width

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Represents a class rectangle."""
+
+
+class Rectangle:
+    """"Represents a class rectangle define by width and height .
+    Private instance attribute: width
+    Instantiation with optional wiidth and height."""
+    def __init__(self, width=0, height=0):
+        """ Initialise the data."""
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """ Retrieves the size."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Sets the width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Retrieves the data."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Sets the data."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
