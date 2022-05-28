3555df337505c81da5f8f7794711520c34924e85
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-28 15:26:01 +1000

    fix: add docs and resolve typo

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -6,9 +6,9 @@ import math
 
 
 class MagicClass:
-    """Initializes the class""
+    """Represents a circle"""
     def __init__(self, radius=0):
-       
+        """Initializes the class"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
