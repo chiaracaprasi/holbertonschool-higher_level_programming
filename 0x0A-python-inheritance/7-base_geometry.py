82b7407ae613c3a6d2f025cdef772387a1ea410d
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-29 22:05:27 -0700

    fix: change isinstace to type

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,7 +15,7 @@ class BaseGeometry:
         """
         validates value
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
