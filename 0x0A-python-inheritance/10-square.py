fb2033fdd8cf621a8811c9062fa8b2164ce15a4c
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-29 22:33:24 -0700

    build: add file for task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 10-square
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines a subclass of Rectangle"""
+    def __init__(self, size):
+        """instantiation of the rectangle"""
+        self.integer_validator("size", size)
+        self.size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ Returns the area."""
+        return self.size ** 2
