8c39a9a584255ecbdea624b25b08626c29c13bd4
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-29 21:07:15 -0700

    build: add script for task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 5-base_geometry
+"""
+
+
+class BaseGeometry:
+    """Defines a class"""
+
+    def area(self):
+        """ raises an Exception """
+        raise Exception('area() is not implemented')
