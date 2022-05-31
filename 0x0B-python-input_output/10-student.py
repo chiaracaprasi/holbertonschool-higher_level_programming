35533efb5b669b1f58aabc55b3877ecbee3768c0
Chiara Caprasi <caprasichiara@gmail.com>
2022-05-31 01:36:26 -0700

    build: add script for task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Module 9-student
+"""
+
+
+class Student():
+    """ Defines a class"""
+    def __init__(self, first_name, last_name, age):
+        """ initialiase data """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ returns the dictionary description with simple data structure
+        (list, dictionary, string, integer and boolean)
+        for JSON serialization of an object: """
+
+        my_dict = {}
+        if attrs is None:
+            return self.__dict__
+
+        if type(attrs) is list or all(type(i) is str for i in attrs):
+            for i in attrs:
+                try:
+                    my_dict[i] = self.__dict__[i]
+                except Exception:
+                    pass
+            return my_dict
