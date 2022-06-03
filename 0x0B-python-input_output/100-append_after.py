4acdba7c65db2b46299de76788e4024a64f3887d
Chiara Caprasi <caprasichiara@gmail.com>
2022-06-02 21:29:48 -0700

    build: add 100-append_after

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains one function
+"""
+import json
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """" Append new_string after search_string in filename """
+    with open(filename, 'r', encoding='utf-8') as read_file:
+        text = read_file.readlines()
+
+    with open(filename, 'w', encoding='utf-8') as write_file:
+        my_string = ""
+        for line in text:
+            if search_string in line:
+                my_string = my_string + new_string
+            my_string = my_string + line
+        write_file.write(my_string)
