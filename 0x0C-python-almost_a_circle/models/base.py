d6af70a917d8844d5df413b9d0df01b31f738659
Chiara Caprasi <caprasichiara@gmail.com>
2022-06-06 21:55:19 -0700

    build: add cvs func

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -67,3 +67,46 @@ class Base:
         except Exception:
             pass
         return my_list
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ Save to CSV file """
+        filename = cls.__name__ + '.csv'
+        with open(filename, 'w', newline='') as csvfile:
+            if list_objs is None or len(list_objs) == 0:
+                csvfile.write("[]")
+            else:
+                writer = csv.writer(csvfile)
+                if cls.__name__ == "Square":
+                    for o in list_objs:
+                        writer.writerow([o.id, o.size, o.x, o.y])
+                if cls.__name__ == "Rectangle":
+                    for o in list_objs:
+                        writer.writerow([o.id, o.width, o.height, o.x, o.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ Deserialisation in CSV, returns a list of instances"""
+        filename = cls.__name__ + '.csv'
+        my_list = []
+        try:
+            with open(filename, 'r') as cvsfile:
+                reader = csv.reader(csvfile)
+                for i in reader:
+                    if cls.__name__ == "Square":
+                        dictionary = {"id": int(args[0]),
+                                      "size": int(args[1]),
+                                      "x": int(args[2]),
+                                      "y": int(args[3])}
+                    elif cls.__name__ == "Rectangle":
+                        dictionary = {"id": int(args[0]),
+                                      "width": int(args[1]),
+                                      "height": int(args[2]),
+                                      "x": int(args[3]),
+                                      "y": int(args[4])}
+
+                    obj = cls.create(**dictionary)
+                    my_list.append(obj)
+                    return my_list
+        except Exception:
+            return my_list
