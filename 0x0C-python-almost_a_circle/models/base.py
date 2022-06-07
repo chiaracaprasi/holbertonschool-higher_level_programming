502ecf13b90ebbb13e34bcf099759ca4dc303c4a
Chiara Caprasi <caprasichiara@gmail.com>
2022-06-06 19:13:33 -0700

    fix: add option for square and rect dummy

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -47,7 +47,10 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """Returns an instance with all attributes already set """
-        dummy = cls(1, 4)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
         dummy.update(**dictionary)
         return dummy
 
