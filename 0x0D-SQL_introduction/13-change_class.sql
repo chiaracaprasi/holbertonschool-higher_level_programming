ba97ab7e964aa42486f96a01b979fd587fb04d68
Chiara Caprasi <caprasichiara@gmail.com>
2022-07-04 22:04:43 -0700

    build: add task 13

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,6 @@
+-- updates the score of Bob to 10 in the table second_table
+-- You are not allowed to use Bob’s id value, only the name field
+-- The database name will be passed as an argument of the mysql command
+
+DELETE FROM second_table
+WHERE score <= 5;
