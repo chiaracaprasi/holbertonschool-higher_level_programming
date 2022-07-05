08cfa7e177f9ac73747d0126b129e7415d4c3e05
Chiara Caprasi <caprasichiara@gmail.com>
2022-07-04 21:29:28 -0700

    build: add task 7

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- inserts a new row in the table first_table
+-- New ro: id = 89, name = Best School
+-- The database name will be passed as an argument of the mysql command
+
+INSERT INTO first_table (`id`, `name`) VALUES (89, 'Best School');
