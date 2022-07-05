090f6d0de5321c0f0187d26a2f94e27272dd7b52
Chiara Caprasi <caprasichiara@gmail.com>
2022-07-04 21:21:53 -0700

    build: add task 6

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- lists all rows of the table first_table from the database
+-- The database name will be passed as an argument of the mysql command
+-- All fields should be printed
+
+SELECT * FROM first_table;
