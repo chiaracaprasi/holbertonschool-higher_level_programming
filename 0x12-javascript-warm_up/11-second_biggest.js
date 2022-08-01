d2d2b8f5cf893420676db88e94db1e1ac7ef6591
Chiara Caprasi <caprasichiara@gmail.com>
2022-07-31 21:56:01 -0700

    build: add task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+function secondHighest (arr) {
+  const result = arr.sort(function (a, b) { return b - a; })[3];
+  return result;
+}
+
+if (process.argv.length === 2) {
+  console.log('0');
+} else if (process.argv.length === 3) {
+  console.log('1');
+} else {
+  const result = secondHighest(process.argv);
+  console.log(result);
+}
