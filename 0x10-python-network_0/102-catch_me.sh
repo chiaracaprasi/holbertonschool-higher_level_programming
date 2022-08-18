1c26bb10009cc7318ba2488c4e19ab86665c39a7
Chiara Caprasi <91517809+chiaracaprasi@users.noreply.github.com>
2022-08-18 21:07:30 +1000

    update 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s -X -L PUT -d "user_id=98" 0.0.0.0:5000/catch_me -H "Origin: HolbertonSchool"
+curl -s -L -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me -H "Origin: HolbertonSchool"
