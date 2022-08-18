0c1664e363ea6f0a723639379d3d23dc7af17129
Chiara Caprasi <91517809+chiaracaprasi@users.noreply.github.com>
2022-08-18 20:55:13 +1000

    Fix: task requirement

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script  takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-RequestHeader set X-HolbertonSchool-User-Id "98"
-curl -s -X GET "$1" -H "X-HolbertonSchool-User-Id: 98"
+curl -H "X-HolbertonSchool-User-Id: 98" "$1"
