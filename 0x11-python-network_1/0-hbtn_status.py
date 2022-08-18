db06138ebf69149fa361a79a28095f5d43bee176
Chiara Caprasi <caprasichiara@gmail.com>
2022-08-18 17:56:29 +1000

    fix: pycodestyle on task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,11 +4,11 @@
 import urllib.request
 
 if __name__ == "__main__":
-   with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-      resp = response.read()
-      html = resp.decode("UTF-8")
-      typ = type(resp)
-      print('Body response:')
-      print(f'\t- type: {typ}')
-      print(f'\t- content: {resp}')
-      print(f'\t- utf8 content: {html}')
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        resp = response.read()
+        html = resp.decode("UTF-8")
+        typ = type(resp)
+        print('Body response:')
+        print(f'\t- type: {typ}')
+        print(f'\t- content: {resp}')
+        print(f'\t- utf8 content: {html}')
