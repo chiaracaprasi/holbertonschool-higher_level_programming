ae8fc6803766e5643ad1cdd42e560df2f85e18e7
Chiara Caprasi <91517809+chiaracaprasi@users.noreply.github.com>
2022-08-18 20:57:06 +1000

    fix: #!

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Script  takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
 curl -H "X-HolbertonSchool-User-Id: 98" "$1"
