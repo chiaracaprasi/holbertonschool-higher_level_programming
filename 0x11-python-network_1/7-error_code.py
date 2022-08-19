ba37584e7d5912cecb98981a3c9931cf9909d2f1
Chiara Caprasi <caprasichiara@gmail.com>
2022-08-19 11:22:19 +1000

    ad task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Sript that takes in a URL and an email,
+sends a request to the URL and
+displays the body of the response """
+from fcntl import F_GET_SEALS
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    url = argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print("Index")
