2c25eff4ce5c9e81dc6b099a099ed64aac99485b
Chiara Caprasi <caprasichiara@gmail.com>
2022-08-19 10:35:00 +1000

    add task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status """
+from requests import get
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    res = get(url)
+    print('Body response:')
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- utf8 content: {}".format(res.text))
