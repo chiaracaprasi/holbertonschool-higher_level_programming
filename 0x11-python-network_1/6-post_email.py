8de515f50952a873038070ce491e318a2eb5e925
Chiara Caprasi <caprasichiara@gmail.com>
2022-08-19 11:07:43 +1000

    add task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Sript that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response """
+import email
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    url = argv[1]
+    email = {'email': argv[2]}
+    # data = urllib.parse.urlencode(values)
+    # data = data.encode('ascii')
+    r = requests.post(url, data=email)
+    print(r.text)
