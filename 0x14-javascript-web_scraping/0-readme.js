990a47b43554dacedcc7cf906acb278228e02237
Chiara Caprasi <caprasichiara@gmail.com>
2022-09-13 12:56:59 +1000

    fix semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 const filename = process.argv[2];
 
-fs.readFile(filename, "utf-8", (err, inputD) => {
+fs.readFile(filename, 'utf-8', (err, inputD) => {
   if (err) {
     console.log(err);
   } else {
