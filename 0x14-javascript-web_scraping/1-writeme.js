6e85a92f9dea8230b0cc64720f86af86a82aa0d9
Chiara Caprasi <caprasichiara@gmail.com>
2022-09-13 13:13:21 +1000

    rm utf8

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const filename = process.argv[2];
 const text = process.argv[3];
 
-fs.writeFile(filename, text, 'utf-8' (err) => {
+fs.writeFile(filename, text, err => {
   if (err) {
     console.log(err);
   }
